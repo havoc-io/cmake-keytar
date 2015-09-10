9eb4b62264c8b1020f53494c56ef839722a6970c
Jacob Howard <jacob@havoc.io>
2015-09-11 02:06:28 +0300

    Disabled test running on Linux for the time being.

--- posix-ci.sh
+++ posix-ci.sh
@@ -16,8 +16,12 @@ cmake --build . || exit $?
 if [ "$TRAVIS_OS_NAME" = "osx" ]; then
   ctest --output-on-failure || exit $?
 else
-  # HACK: Have to use a virtual X11 environment for GNOME code
-  xvfb ctest --output-on-failure || exit $?
+  # TODO: Run tests for Linux on Travis CI once we can figure out how to talk to
+  # gnome-keyring-daemon.  node-keytar hasn't figured it out either.  I think
+  # that the daemon needs to be run before login to be able to unlock our login
+  # keychain, but I don't see how that's possible with Travis CI.  For now, we
+  # can at least check that things compile correctly.
+  echo "Skipping tests until gnome-keyring-daemon works on Travis CI"
 fi
 
 # All done

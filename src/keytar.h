fe22603a5ec8df5b581e62cc05cdbbc7c7bc7fa2
Jacob Howard <jacob@havoc.io>
2015-09-11 00:36:57 +0300

    Added symbol exports.

--- src/keytar.h
+++ src/keytar.h
@@ -3,18 +3,31 @@
 
 #include <string>
 
+// Visibility macros
+#ifdef _MSC_VER
+    #define KEYTAR_API __declspec(dllexport)
+#elif defined(__GNUC__) && __GNUC__ >= 4
+    #define KEYTAR_API __attribute__((visibility("default")))
+#else
+    #define KEYTAR_API
+#endif
+
 namespace keytar {
 
+KEYTAR_API
 bool AddPassword(const std::string& service,
                  const std::string& account,
                  const std::string& password);
 
+KEYTAR_API
 bool GetPassword(const std::string& service,
                  const std::string& account,
                  std::string* password);
 
+KEYTAR_API
 bool DeletePassword(const std::string& service, const std::string& account);
 
+KEYTAR_API
 bool FindPassword(const std::string& service, std::string* password);
 
 }  // namespace keytar
